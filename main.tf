01b6447079d7df494e77f2402d95d504b0ea6cf4
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2022-01-03 14:19:47 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,9 +4,7 @@ terraform {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "2.90.0"
-      configuration_aliases = [
-        azurerm.vhub
-      ]
+
     }
     azuread = {
       source  = "hashicorp/azuread"

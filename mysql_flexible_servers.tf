8682ab31be3e06b94c3932fc108896f326ccf7f6
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2022-02-01 17:49:05 +0200

    Update mysql_flexible_servers.tf

--- mysql_flexible_servers.tf
+++ mysql_flexible_servers.tf
@@ -1,11 +1,11 @@
-output "mysql_flexible_servers" {
-  value = module.mysql_flexible_servers
+output "mysql_flexible_server" {
+  value = module.mysql_flexible_server
 }
 
-module "mysql_flexible_servers" {
+module "mysql_flexible_server" {
   source     = "./modules/databases/mysql_flexible_server"
   depends_on = [module.keyvaults, module.networking]
-  for_each   = local.database.mysql_flexible_servers
+  for_each   = local.database.mysql_flexible_server
 
   global_settings = local.global_settings
   client_config   = local.client_config

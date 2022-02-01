03227cfd78e0c6e7ca15ac66cf4d7285a42f8f7e
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2022-02-01 19:26:05 +0200

    Update output.tf

--- modules/databases/mysql_flexible_server/output.tf
+++ modules/databases/mysql_flexible_server/output.tf
@@ -3,11 +3,11 @@ output "location" {
   value       = var.resource_group.location
 }
 
-output "mysql_flexible_server_administrator_username" {
-  description = "Administrator username of MYSQL flexible server"
-  value       = azurerm_mysql_flexible_server.mysql.administrator_login
-  sensitive   = true
-}
+#output "mysql_flexible_server_administrator_username" {
+#  description = "Administrator username of MYSQL flexible server"
+#  value       = azurerm_mysql_flexible_server.mysql.administrator_login
+#  sensitive   = true
+#}
 
 output "mysql_flexible_server_administrator_password" {
   description = "Administrator password of MYSQL flexible server"
@@ -35,26 +35,26 @@ output "mysql_flexible_server_public_network_access_enabled" {
   value       = azurerm_mysql_flexible_server.mysql.public_network_access_enabled
 }
 
-output "mysql_flexible_server_configuration_id" {
-  description = "ID of the MYSQL flexible server configuration"
-  value = {
-    for k, v in azurerm_mysql_flexible_server_configuration.mysql : k => v.id
-  }
-}
+#output "mysql_flexible_server_configuration_id" {
+#  description = "ID of the MYSQL flexible server configuration"
+ # value = {
+ #   for k, v in azurerm_mysql_flexible_server_configuration.mysql : k => v.id
+#  }
+#}
 
-output "mysql_flexible_server_database_id" {
-  description = "ID of the MYSQL flexible server database"
-  value = {
-    for k, v in azurerm_mysql_flexible_server_database.mysql : k => v.id
-  }
-}
+#output "mysql_flexible_server_database_id" {
+##  description = "ID of the MYSQL flexible server database"
+#  value = {
+#    for k, v in azurerm_mysql_flexible_server_database.mysql : k => v.id
+#  }
+#}
 
-output "mysql_flexible_server_firewall_rule_id" {
-  description = "ID of the MYSQL flexible server firewall rule"
-  value = {
-    for k, v in azurerm_mysql_flexible_server_firewall_rule.mysql : k => v.id
-  }
-}
+#output "mysql_flexible_server_firewall_rule_id" {
+#  description = "ID of the MYSQL flexible server firewall rule"
+#  value = {
+#    for k, v in azurerm_mysql_flexible_server_firewall_rule.mysql : k => v.id
+#  }
+#}
 
 output "resource_group_name" {
   description = "Name of the Resource Group where the resource exists."

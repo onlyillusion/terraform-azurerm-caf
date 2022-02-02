74a3cf7cbdd8ccc87c3547a04e3cf917ede32691
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2022-02-02 19:07:32 +0200

    Update firewall_rule.tf

--- modules/databases/mysql_flexible_server/firewall_rule.tf
+++ modules/databases/mysql_flexible_server/firewall_rule.tf
@@ -3,7 +3,7 @@
 resource "azurerm_mysql_flexible_server_firewall_rule" "mysql" {
   for_each = try(var.settings.mysql_firewall_rules, {})
 
-  name                = each.value.
+  name                = each.value.name
   server_name         = azurerm_mysql_flexible_server.mysql.name
   start_ip_address    = each.value.start_ip_address
   end_ip_address      = each.value.end_ip_address

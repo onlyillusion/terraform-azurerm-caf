83e15dc3ab5fd621afce8008b18516b0c6a20a3a
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2022-02-02 18:47:52 +0200

    Update database.tf

--- modules/databases/mysql_flexible_server/database.tf
+++ modules/databases/mysql_flexible_server/database.tf
@@ -1,14 +1,15 @@
 
 
 
-resource "azurerm_mysql_flexible_server_database" "mysql" {
+resource "azurerm_mysql_flexible_database" "mysql" {
   depends_on = [azurerm_mysql_flexible_server.mysql]
   for_each   = try(var.settings.mysql_databases, {})
 
-  name      = azurerm_mysql_flexible_server_database[each.key].name
-  server_id = azurerm_mysql_flexible_server.mysql.id
+  name      = azurerm_mysql_flexible_database[each.key].name
+  server_name = azurerm_mysql_flexible_server.mysql.name
   collation = try(each.value.collation, "en_US.utf8")
   charset   = try(each.value.charset, "utf8")
+  resource_group_name = var.resource_group.name
 }
 
 resource "azurerm_key_vault_secret" "mysql_database_name" {

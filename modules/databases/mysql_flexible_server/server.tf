bff45ae2f1449afd051bcf807bbaad62247f534b
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2022-02-03 15:45:08 +0200

    Update server.tf

--- modules/databases/mysql_flexible_server/server.tf
+++ modules/databases/mysql_flexible_server/server.tf
@@ -1,6 +1,8 @@
 
 
 resource "azurerm_mysql_flexible_server" "mysql" {
+  depends_on = [azurerm_key_vault_secret.mysql_administrator_password]
+  
   name                = var.settings.name
   resource_group_name = var.resource_group.name
   location            = var.resource_group.location

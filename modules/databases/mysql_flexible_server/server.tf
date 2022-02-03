76d5302a501c93c35a40f835e5877529efb0be0d
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2022-02-03 15:52:24 +0200

    Update server.tf

--- modules/databases/mysql_flexible_server/server.tf
+++ modules/databases/mysql_flexible_server/server.tf
@@ -19,7 +19,7 @@ resource "azurerm_mysql_flexible_server" "mysql" {
   source_server_id                  = try(var.settings.create_mode, "PointInTimeRestore") == "PointInTimeRestore" ? try(var.settings.source_server_id, null) : null
 
   administrator_login    = try(var.settings.create_mode, "Default") == "Default" ? try(var.settings.administrator_username, "psqladmin") : null
-  administrator_password = try(var.settings.create_mode, "Default") == "Default" ? try(var.settings.administrator_password,  azurerm_key_vault_secret.mysql_administrator_password.0.value) : null
+  administrator_password = try(var.settings.administrator_password, azurerm_key_vault_secret.mysql_administrator_password.0.value)
 
   backup_retention_days = try(var.settings.backup_retention_days, null)
 

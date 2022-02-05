8bc48c26451a5f5da5bbaa9c5556438847988abf
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2022-02-05 16:32:18 +0200

    Update configuration.tf

--- modules/databases/mysql_flexible_server/configuration.tf
+++ modules/databases/mysql_flexible_server/configuration.tf
@@ -1,10 +1,10 @@
-
+/*
 resource "time_sleep" "server_configuration" {
   depends_on = [azurerm_mysql_flexible_server.mysql]
 
   create_duration = "120s"
 }
-
+*/
 resource "azurerm_mysql_flexible_server_configuration" "mysql" {
   depends_on = [time_sleep.server_configuration]
   for_each   = try(var.settings.mysql_configurations, {})

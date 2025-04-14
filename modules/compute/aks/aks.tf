a6949bc32588b3ce6cee7d09b6eff1ddf3eeb8b0
onlyillusion <onlyilussion@gmail.com>
2025-04-14 13:21:17 +0300

    AKS: FIX nodepools upgrade_settings

--- modules/compute/aks/aks.tf
+++ modules/compute/aks/aks.tf
@@ -563,9 +563,11 @@ resource "azurerm_kubernetes_cluster_node_pool" "nodepools" {
   scale_down_mode              = try(each.value.scale_down_mode, null)
   ultra_ssd_enabled            = try(each.value.ultra_ssd_enabled, false)
   dynamic "upgrade_settings" {
-    for_each = try(each.value.upgrade_settings, null) == null ? [] : [1]
+    for_each = try(each.value.upgrade_settings[*], {})
     content {
-      max_surge = upgrade_settings.value.max_surge
+      drain_timeout_in_minutes      = try(upgrade_settings.value.drain_timeout_in_minutes, null)
+      node_soak_duration_in_minutes = try(upgrade_settings.value.node_soak_duration_in_minutes, null)
+      max_surge                     = upgrade_settings.value.max_surge
     }
   }
 
